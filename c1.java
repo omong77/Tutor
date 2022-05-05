9429e4691d69880ae86dc6a609c1e58c51dc6b9f
omong77 <100646518+omong77@users.noreply.github.com>
2022-05-05 17:45:18 -0500

    Add files via upload

---
+++ c1.java
@@ -0,0 +1,11 @@
+public class c1 {
+
+   public static void main(String args[]) {
+      String A = "  A A A A";
+      System.out.println(A);
+      System.out.println("     A");
+      System.out.println("   A  A");
+      System.out.println(A);
+      System.out.println(" A       A");
+   }
+}
